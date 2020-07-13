8b464a4e0d959cb83b7d844eda0b05eed9c86590
Anna Diederich <annadiederich@Annas-MacBook-Pro.local>
2020-07-13 12:17:43 -0500

    changes

---
+++ QuizApp/QuizApp/QuestionScreen.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionScreen.swift
+//  QuizApp
+//
+//  Created by Anna Diederich on 7/13/20.
+//  Copyright © 2020 Olivia Edwards. All rights reserved.
+//
+
+import UIKit
+
+class QuestionScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
